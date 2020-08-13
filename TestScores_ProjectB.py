fe399d766e9a35f51b223d6e21f4bf4bebd8cac9
adisalunkhe08 <69620213+adisalunkhe08@users.noreply.github.com>
2020-08-12 23:40:29 -0700

    Add files via upload

---
+++ TestScores_ProjectB.py
@@ -0,0 +1,9 @@
+student1 = int(input("Enter test score for student 1 "))
+student2 = int(input("Enter test score for student 2 "))
+student3 = int(input("Enter test score for student 3 "))
+student4 = int(input("Enter test score for student 4 "))
+student5 = int(input("Enter test score for student 5 "))
+
+average = (student1 + student2 + student3 +student4 +student5)/5
+print ("The average score is ", average)
+              
